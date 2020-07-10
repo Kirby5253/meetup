6dfcaa947637c96b4264af482c0e5a1bb08d7218
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-10 15:26:38 -0500

    cleared suggestions on click

--- src/Components/CitySearch.js
+++ src/Components/CitySearch.js
@@ -13,8 +13,9 @@ class CitySearch extends Component {
 		getSuggestions(value).then((suggestions) => this.setState({ suggestions }));
 	};
 
-	handleItemClicked = (value) => {
-		this.setState({ query: value });
+	handleItemClicked = (value, lat, lon) => {
+		this.setState({ query: value, suggestions: [] });
+		this.props.updateEvents(lat, lon);
 	};
 
 	render() {
@@ -23,7 +24,10 @@ class CitySearch extends Component {
 				<input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged} />
 				<ul className="suggestions">
 					{this.state.suggestions.map((item) => (
-						<li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string)}>
+						<li
+							key={item.name_string}
+							onClick={() => this.handleItemClicked(item.name_string, item.lat, item.lon)}
+						>
 							{item.name_string}
 						</li>
 					))}
