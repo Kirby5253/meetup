a4990b8dfe290ce4b0df57bfc5a51220b382e3f8
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-10 17:21:38 -0500

    imported better mock data

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../Components/EventList';
 import Event from '../Components/Event';
+import { mockEvents } from '../mock-events';
 
 describe('<EventList /> component', () => {
 	test('render correct number of events', () => {
-		const EventListWrapper = shallow(<EventList events={[ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 } ]} />);
-		expect(EventListWrapper.find(Event)).toHaveLength(4);
+		const EventListWrapper = shallow(<EventList 
+			events={mockEvents.events} />);
+		expect(EventListWrapper.find(Event)).toHaveLength(20);
 	});
 });
