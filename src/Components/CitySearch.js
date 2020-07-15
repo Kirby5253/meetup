fead862ffe225caee3788dab2cc1bbd82ff2fb36
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-15 15:51:13 -0500

    Implemented alerts to city search and number of events

--- src/Components/CitySearch.js
+++ src/Components/CitySearch.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { getSuggestions } from '../api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 	state = {
 		query: '',
-		suggestions: []
+		suggestions: [],
+		infoText: ''
 	};
 
 	handleInputChanged = (event) => {
 		const value = event.target.value;
 		this.setState({ query: value });
 		getSuggestions(value).then((suggestions) => this.setState({ suggestions }));
+
+		if (value && this.state.suggestions.length === 0) {
+			this.setState({
+				infoText: 'We can not find the city you are looking for. Please try another city',
+			});
+		} else {
+			this.setState({
+				infoText: ''
+			})
+		}
 	};
 
 	handleItemClicked = (value, lat, lon) => {
@@ -21,6 +33,7 @@ class CitySearch extends Component {
 	render() {
 		return (
 			<div className="CitySearch">
+				<InfoAlert text = {this.state.infoText} />
 				<input 
 					placeholder="Search by City or Zip Code" 
 					type="text" 
--- src/Components/NumberOfEvents.js
+++ src/Components/NumberOfEvents.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
+	state = {
+		infoText: ''
+	}
+
 	handleInputChanged = (event) => {
 		const value = event.target.value;
 		this.props.updateEventsNumber(value);
-	};
+
+		if( value < 1) {
+			this.setState({
+				infoText: 'Please use a number greater than 0 to see results.',
+			});
+		} else {
+			this.setState({
+				infoText: ''
+			})
+		}
+	}
 
 	render() {
 		return (
 			<div className="numberOfEvents">
+				<ErrorAlert text = {this.state.infoText} />
 				Show{' '}
 				<input
 					type="number"
-					min="1"
+					min="0"
 					className="showEventsNumber"
 					value={this.props.eventsNumber}
 					onChange={this.handleInputChanged}
